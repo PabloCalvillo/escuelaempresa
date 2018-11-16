12bf09888909be24dd2cf60a7a13ccf93c093581
Saul Dominguez <saulydominguez@gmail.com>
2018-11-16 09:48:24 +0100

    Added tabs to grades and buttons to specific petition types

--- resources/views/grades/editGrade.blade.php
+++ resources/views/grades/editGrade.blade.php
@@ -21,31 +21,71 @@
 		</form>
 	</div>
 	<div class="col-md-8 col-md-offset-2" style="margin-top:20px;">
-		@forelse ($petitionTypes as $type => $petitions)
-			@if ($petitions->count() > 0)
-				<h2 class="text-center text-mute"> {{ __("Solicitudes de ") }} {{ $type }}</h2>
+		@if ($petitionTypes)
+			<a href="./pdfGradesTypes/{{ $grade->id }}" class="btn btn-info pull-right"> {{ __("Descargar PDF general") }}</a>
 
-				@foreach ($petitions as $petition)
-					<div class="panel panel-default">
-						<div class="panel-heading" style="display: flex; align-items: center; justify-content: flex-end;">
-							<span style="margin-right: auto;">
-								<a href="{{ route('companyEditForm', $petition->company->id) }}" style="margin-right: auto;"> {{ $petition->company->name }}</a> quiere {{ $petition->n_students }}
-								estudiante{{ $petition->n_students == 1 ? '' : 's' }} </a>
-							</span>
-							
-						</div>
-					</div>
+			<ul class="nav nav-tabs">
+				@foreach ($petitionTypes as $type => $petitions)
+					<li role="presentation" data-tab="{{ $type }}"><a href="#">{{ $type }}</a></li>
 				@endforeach
-			@endif
-		@empty
+			</ul>
+
+			@foreach ($petitionTypes as $type => $petitions)
+				@if ($petitions->count() > 0)
+					<div id="petition-container-{{ $type }}">
+						<h2 class="text-center text-mute"> {{ __("Solicitudes de ") }} {{ $type }}</h2>
+
+						@foreach ($petitions as $petition)
+							<div class="panel panel-default">
+								<div class="panel-heading" style="display: flex; align-items: center; justify-content: flex-end;">
+									<span style="margin-right: auto;">
+										<a href="{{ route('companyEditForm', $petition->company->id) }}" style="margin-right: auto;"> {{ $petition->company->name }}</a> quiere {{ $petition->n_students }}
+										estudiante{{ $petition->n_students == 1 ? '' : 's' }} </a>
+									</span>
+									
+								</div>
+							</div>
+						@endforeach
+
+						<a href="./pdfGradesTypes/{{ $grade->id }}/{{ $type }}" class="btn btn-info pull-right"> {{ __("Descargar PDF") }}</a>
+					</div>
+				@endif
+			@endforeach
+		@else
 			<div class="alert alert-danger">
 				{{ __("No hay ningúna solicitud en este momento") }}
 			</div>
-		@endforelse
-		
-		@if( $petitions->count() > 0 )
-			<a href="./pdfGradesTypes/{{ $grade->id }}" class="btn btn-info pull-right"> {{ __("Descargar PDF") }}</a>
 		@endif
 	</div>
 </div>
+
+<script>
+	document.body.onload = function(){
+		var lis = document.querySelectorAll("ul.nav.nav-tabs li")
+		var activeTabContainer = document.getElementById(`petition-container-${lis[0].dataset.tab}`)
+		var activeLi = lis[0]
+
+		activeTabContainer.classList.add("show")
+		activeLi.classList.add("active")
+
+		lis.forEach(li => {
+			var tabLink = li.getElementsByTagName("a")[0]
+			var tabContainer = document.getElementById(`petition-container-${li.dataset.tab}`)
+			
+			tabLink.onclick = function(ev){
+				if ( activeTabContainer )
+					activeTabContainer.classList.remove("show")
+
+				if ( activeLi )
+					activeLi.classList.remove("active")
+					
+				tabContainer.classList.add("show")
+				li.classList.add("active")
+				
+				activeTabContainer = tabContainer
+				activeLi = li
+			}
+		})
+	}
+</script>
 @endsection
